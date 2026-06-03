9b4906e5cc97e9f71085671bbd8919a298f573a5
konstantine2121 <kach89kach89@gmail.com>
2026-06-03 18:43:15 +0300

    Refactor BrandsController. Remove unused usings and attributes.

--- src/Services/Catalog/Catalog.API/Controllers/BrandsController.cs
+++ src/Services/Catalog/Catalog.API/Controllers/BrandsController.cs
@@ -1,13 +1,9 @@
 ﻿using Catalog.Application.Queries.BrandQueries;
 using Catalog.Application.Responses.BrandResponses;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Catalog.API.Controllers;
 
-
-[ApiController]
-[Route("api/[controller]")]
 public class BrandsController : ApiController
 {
     [HttpGet]
