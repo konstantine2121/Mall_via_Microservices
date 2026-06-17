18f553dc36bd648e8bfe10c4e11ad38b82661ad0
konstantine2121 <kach89kach89@gmail.com>
2026-06-17 21:27:45 +0300

    Add Validation example to CreateCatalogItemHandler

--- src/Services/Catalog/Catalog.Application/Handlers/CatalogItemHandlers/CreateCatalogItemHandler.cs
+++ src/Services/Catalog/Catalog.Application/Handlers/CatalogItemHandlers/CreateCatalogItemHandler.cs
@@ -5,14 +5,49 @@ using Mapster;
 
 namespace Catalog.Application.Handlers.CatalogItemHandlers;
 
-public class CreateCatalogItemHandler (ICatalogItemRepository catalogItemRepository)
+public class CreateCatalogItemHandler (ICatalogItemRepository catalogItemRepository,
+    IBrandRepository brandRepository,
+    ICategoryRepository categoryRepository)
     : IRequestHandler<CreateCatalogItemCommand, CreateCatalogItemResult>
 {
-    public async Task<CreateCatalogItemResult> Handle(CreateCatalogItemCommand command, CancellationToken cancellationToken)
+    public async Task<CreateCatalogItemResult> Handle(CreateCatalogItemCommand command,
+        CancellationToken cancellationToken)
     {
+        // Валидация целостности для бренда и категории
+        // await ValidateBrandExists(command.Brand, cancellationToken);
+        // await ValidateCategoryExists(command.Category, cancellationToken);
+
         var catalogItem = command.Adapt<CatalogItem>();
         catalogItem.Id = Guid.NewGuid();
         await catalogItemRepository.CreateCatalogItemAsync(catalogItem, cancellationToken);
         return new CreateCatalogItemResult(catalogItem.Id);
+    }
+
+    private async Task ValidateCategoryExists(Category? category, CancellationToken cancellationToken)
+    {
+        // Проверяем Category
+        var categoryExists = category != null
+                             && (await categoryRepository.GetAllCategoriesAsync(cancellationToken))
+                             .Any(c => c.Id == category.Id);
+
+        if (!categoryExists)
+        {
+            throw new ArgumentException("Указанная категория не существует");
+            //Следует использовать свои конкретные типы исключений
+        }
+    }
+
+    private async Task ValidateBrandExists(Brand? brand, CancellationToken cancellationToken)
+    {
+        // Проверяем Brand
+        var brandExists = brand != null
+                          && (await brandRepository.GetAllBrandsAsync(cancellationToken))
+                          .Any(b => b.Id == brand.Id);
+
+        if (!brandExists)
+        {
+            throw new ArgumentException("Указанный бренд не существует");
+            //Следует использовать свои конкретные типы исключений
+        }
     }
 }
